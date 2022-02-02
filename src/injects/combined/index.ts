79bc8f4f85db1561fc66ff5234706c9304452a93
0xnewton <0xnewton@protonmail.com>
2022-02-02 00:41:53 -0500

    remove unnecessary comments

--- src/injects/combined/index.ts
+++ src/injects/combined/index.ts
@@ -1,8 +1,4 @@
-// import { inject as injectWalletButton } from '../button/injection'
-// import { inject as injectSwap } from '../swap/injection'
 import { inject as injectCrowdSale } from '../crowdSale/injection'
-
 ;(() => {
-  // injectWalletButton()
   injectCrowdSale()
 })()
