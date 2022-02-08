da1b7ff3724da7ae5f5457958b7877088b010a9a
0xnewton <0xnewton@protonmail.com>
2022-02-07 19:02:14 -0500

    fix exponential precision issue on large purchases

--- src/lib/components/CrowdSale/helpers.ts
+++ src/lib/components/CrowdSale/helpers.ts
@@ -6,5 +6,5 @@ export const parseWei = (amount: string, decimals: number | undefined): string =
   if (decimals == undefined) {
     decimals = DEFAULT_DECIMALS
   }
-  return new BN(amount).multipliedBy(new BN(10).pow(decimals)).toString()
+  return new BN(amount).multipliedBy(new BN(10).pow(decimals)).toFixed()
 }
