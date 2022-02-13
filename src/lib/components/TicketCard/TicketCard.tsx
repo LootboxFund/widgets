866c770f42e08e445889ccfa4ea3482ba65ac9ba
0xnewton <0xnewton@protonmail.com>
2022-02-13 12:17:51 -0500

    add skeleton

--- src/lib/components/TicketCard/TicketCard.tsx
+++ src/lib/components/TicketCard/TicketCard.tsx
@@ -11,25 +11,22 @@ const TicketCard = ({ ticketID }: TicketCardProps) => {
   const snap = useSnapshot(ticketCardState)
   const stateID = ticketID && snap.lootboxAddress && generateStateID(snap.lootboxAddress, ticketID)
   const ticket = stateID && snap.tickets[stateID] ? snap.tickets[stateID] : undefined
-  if (!ticket || !ticket.metadata?.backgroundImage || !ticket.metadata?.image || !ticket.metadata?.backgroundColor) {
-    return <>LOADING</>
-  }
   return (
-    <$TicketCardContainer backgroundImage={ticket.metadata?.backgroundImage}>
+    <$TicketCardContainer backgroundImage={ticket?.metadata?.backgroundImage}>
       <$TicketLogo
-        backgroundImage={ticket.metadata?.image}
-        backgroundShadowColor={ticket.metadata?.backgroundColor}
+        backgroundImage={ticket?.metadata?.image}
+        backgroundShadowColor={ticket?.metadata?.backgroundColor}
       ></$TicketLogo>
       <$TicketTag>
-        <$TagText>{ticket.metadata?.name}</$TagText>
+        <$TagText>{ticket?.metadata?.name}</$TagText>
         <$Divider />
-        <$TicketIDText>#{ticket.id}</$TicketIDText>
+        <$TicketIDText>{ticket?.id ? `#${ticket.id}` : ''}</$TicketIDText>
       </$TicketTag>
     </$TicketCardContainer>
   )
 }
 
-export const $TicketCardContainer = styled.section<{ backgroundImage: string }>`
+export const $TicketCardContainer = styled.section<{ backgroundImage?: string | undefined }>`
   width: 100%;
   height: 100%;
   border: 0px solid transparent;
@@ -40,21 +37,23 @@ export const $TicketCardContainer = styled.section<{ backgroundImage: string }>`
   gap: 10px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
   min-height: 512px;
-  background: ${(props) => `url("${props.backgroundImage}")`};
+  background: #f5f5f5;
+  ${(props) => (props.backgroundImage ? `background: url("${props.backgroundImage}");` : '')}
   background-size: cover;
 `
 
-export const $TicketLogo = styled.div<{ backgroundImage: string; backgroundShadowColor: string }>`
+export const $TicketLogo = styled.div<{ backgroundImage?: string; backgroundShadowColor?: string }>`
   width: 100%;
   height: 100%;
-  max-width: 256px;
-  max-height: 256px;
+  max-width: ${(props) => (props.backgroundImage ? '256px' : '186px')};
+  max-height: ${(props) => (props.backgroundImage ? '256px' : '186px')};
   border: 0px solid transparent;
   border-radius: 50%;
   margin: auto auto 0px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
-  background: ${(props) => `url("${props.backgroundImage}")`};
-  filter: drop-shadow(0px 0px 40px ${(props) => props.backgroundShadowColor});
+  background: rgba(0, 0, 0, 0.05);
+  ${(props) => (props.backgroundImage ? `background: url("${props.backgroundImage}");` : '')}
+  filter: drop-shadow(0px 0px 40px ${(props) => props.backgroundShadowColor || '#000'});
   background-size: cover;
 `
 
@@ -96,7 +95,7 @@ export const $TicketIDText = styled.p`
 
 const $Divider = styled.div`
   margin: 10px;
-  border: 5px solid rgba(255, 255, 255, 0.33);
+  border: 3px solid rgba(255, 255, 255, 0.33);
   transform: rotate(0deg);
 `
 
