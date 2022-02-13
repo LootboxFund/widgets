c71226c0b0401a3180c3b7ac1eb50fa1d9ee8205
0xnewton <0xnewton@protonmail.com>
2022-02-13 00:25:03 -0500

    fix hmtl warning about uncontroled prop

--- src/lib/components/BuyShares/Input/ShareOutput.tsx
+++ src/lib/components/BuyShares/Input/ShareOutput.tsx
@@ -19,24 +19,10 @@ export interface ShareOutputProps {
 
 const ShareOutput = (props: ShareOutputProps) => {
   const snap = useSnapshot(buySharesState)
-  const snapUserState = useSnapshot(userState)
   const { screen } = useWindowSize()
-  const selectToken = async () => {
-    // if (!props.selectDisabled) {
-    //   buySharesState.targetToken = props.targetToken
-    //   buySharesState.route = '/search'
-    // }
-  }
-  const setQuantity = (quantity: string) => {
-    if (quantity?.length === 0) {
-      buySharesState.inputToken.quantity = undefined
-    } else {
-      buySharesState.inputToken.quantity = !isNaN(parseFloat(quantity)) ? quantity : '0'
-    }
-  }
 
   const sharesDecimals = snap.lootbox.data?.sharesDecimals
-  const quantity = snap.lootbox.quantity
+  const quantity: string = snap.lootbox.quantity || '0'
   const sharesSoldCount = snap.lootbox?.data?.sharesSoldCount
   const quantityBN = quantity && sharesDecimals && new BN(quantity).multipliedBy(new BN(10).pow(sharesDecimals))
   const totalShares = sharesSoldCount && quantityBN && new BN(sharesSoldCount).plus(quantityBN)
@@ -48,26 +34,11 @@ const ShareOutput = (props: ShareOutputProps) => {
     <$TokenInput screen={screen}>
       <$Horizontal flex={1}>
         <$Vertical flex={screen === 'desktop' ? 3 : 2}>
-          <$Input value={quantity} type="number" placeholder="0.00" screen={screen} min={0}></$Input>
+          <$Input readOnly value={quantity} type="number" placeholder="0.00" screen={screen} min={0}></$Input>
           <$FineText screen={screen}>{`Receive Shares (${percentageShares}%* of Earnings)`}</$FineText>
         </$Vertical>
         <$Vertical flex={1}>
           {props.lootbox ? (
-            // <div
-            //   style={{
-            //     height: '30px',
-            //     fontSize: screen === 'desktop' ? '1rem' : '0.9rem',
-            //     fontWeight: 'bold',
-            //     padding: screen === 'desktop' ? '5px 20px' : '5px 10px',
-            //     display: 'flex',
-            //     flexDirection: 'row',
-            //     alignItems: 'center',
-            //     justifyContent: 'center',
-            //     ...(props.selectDisabled && { cursor: 'not-allowed' }),
-            //   }}
-            // >
-
-            // </div>
             <$TokenSymbol screen={screen}>{props.lootbox.name}</$TokenSymbol>
           ) : (
             <$TokenSymbol screen={screen}>loading...</$TokenSymbol>
