5595cf7558318f5f0fdb4bc9979fae662f26201c
0xnewton <0xnewton@protonmail.com>
2022-02-13 05:19:40 -0500

    add ticket card styling

--- src/lib/api/storage.ts
+++ src/lib/api/storage.ts
@@ -1,19 +1,42 @@
-import { ITicketMetadata } from 'lib/types'
+import { ITicketMetadata, TicketID, Address } from 'lib/types'
+import {
+  DEFAULT_CHAIN_ID_HEX,
+  storageUrl,
+  DEFAULT_TICKET_IMAGE,
+  DEFAULT_TICKET_BACKGROUND,
+  DEFAULT_TICKET_BACKGROUND_COLOR,
+} from 'lib/hooks/constants'
+import { userState } from 'lib/state/userState'
+
+const lootboxUrl = (lootboxAddress: Address) =>
+  `${storageUrl(userState.network.currentNetworkIdHex || DEFAULT_CHAIN_ID_HEX)}/lootbox/${lootboxAddress}`
+
+const metadataStorageUrl = (lootboxAddress: Address, ticketID: TicketID) =>
+  `${lootboxUrl(lootboxAddress)}/lootbox/${lootboxAddress}/ticket/${ticketID}.json?alt=media`
+
+const defaultMetadataStorageUrl = (lootboxAddress: Address) =>
+  `${lootboxUrl(lootboxAddress)}/lootbox/${lootboxAddress}/ticket/default.json?alt=media`
 
 export const readJSON = async <T>(file: string): Promise<T> => {
   // TODO: dynamically read json file in cloud storage
   const result: any = {
-    name: '',
+    name: 'Artemis Guild Demo',
     description: '',
-    image: '',
-    backgroundColor: '',
-    backgroundImage: '',
+    image: DEFAULT_TICKET_IMAGE,
+    backgroundColor: DEFAULT_TICKET_BACKGROUND_COLOR,
+    backgroundImage: DEFAULT_TICKET_BACKGROUND,
   }
   return result as T
 }
 
-export const readTicketMetadata = async (file: string): Promise<ITicketMetadata> => {
-  const { name, description, image, backgroundColor, backgroundImage } = await readJSON(file)
+export const readTicketMetadata = async (lootboxAddress: Address, ticketID: TicketID): Promise<ITicketMetadata> => {
+  const defaultFilepath = defaultMetadataStorageUrl(lootboxAddress)
+  const filepath = metadataStorageUrl(lootboxAddress, ticketID)
+  // check if filepath exists
+  //   const fileExists =
+  console.log('reading ticket metadata', filepath)
+
+  const { name, description, image, backgroundColor, backgroundImage } = await readJSON(filepath)
 
   return { name, description, image, backgroundColor, backgroundImage }
 }
--- src/lib/components/TicketCard/TicketCard.tsx
+++ src/lib/components/TicketCard/TicketCard.tsx
@@ -1,4 +1,5 @@
 import react from 'react'
+import styled from 'styled-components'
 import { useSnapshot } from 'valtio'
 import { ticketCardState, generateStateID } from './state'
 
@@ -9,11 +10,95 @@ export interface TicketCardProps {
 const TicketCard = ({ ticketID }: TicketCardProps) => {
   const snap = useSnapshot(ticketCardState)
   const stateID = ticketID && snap.lootboxAddress && generateStateID(snap.lootboxAddress, ticketID)
-  const ticket = stateID && snap.tickets[stateID]
+  const ticket = stateID && snap.tickets[stateID] ? snap.tickets[stateID] : undefined
 
-  console.log('found ticket', ticket)
-
-  return <>hello</>
+  if (!ticket || !ticket.metadata?.backgroundImage || !ticket.metadata?.image || !ticket.metadata?.backgroundColor) {
+    return <>LOADING</>
+  }
+  return (
+    <$TicketCardContainer backgroundImage={ticket.metadata?.backgroundImage}>
+      <$TicketLogo
+        backgroundImage={ticket.metadata?.image}
+        backgroundShadowColor={ticket.metadata?.backgroundColor}
+      ></$TicketLogo>
+      <$TicketTag>
+        <$TagText>{ticket.metadata?.name}</$TagText>
+        <$Divider />
+        <$TicketIDText>#{ticket.id}</$TicketIDText>
+      </$TicketTag>
+    </$TicketCardContainer>
+  )
 }
+
+export const $TicketCardContainer = styled.section<{ backgroundImage: string }>`
+  width: 100%;
+  height: 100%;
+  border: 0px solid transparent;
+  border-radius: 20px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  min-height: 512px;
+  background: ${(props) => `url("${props.backgroundImage}")`};
+  background-size: cover;
+`
+
+export const $TicketLogo = styled.div<{ backgroundImage: string; backgroundShadowColor: string }>`
+  width: 100%;
+  height: 100%;
+  max-width: 256px;
+  max-height: 256px;
+  border: 0px solid transparent;
+  border-radius: 50%;
+  margin: auto auto 0px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+  background: ${(props) => `url("${props.backgroundImage}")`};
+  filter: drop-shadow(0px 0px 40px ${(props) => props.backgroundShadowColor});
+  background-size: cover;
+`
+
+export const $TicketTag = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  margin: auto 0px 20px;
+`
+
+export const $TagText = styled.p`
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 22px;
+  line-height: 30px;
+  text-align: right;
+  position: relative;
+  color: #ffffff;
+  width: 100%;
+  max-width: 50%;
+`
+export const $TicketIDText = styled.p`
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 42px;
+  line-height: 42px;
+  text-align: center;
+  position: relative;
+  color: #ffffff;
+  width: 100%;
+  max-width: 50%;
+  margin: auto;
+`
+
+const $Divider = styled.div`
+  margin: 10px;
+  border: 5px solid rgba(255, 255, 255, 0.33);
+  transform: rotate(0deg);
+`
 
 export default TicketCard
