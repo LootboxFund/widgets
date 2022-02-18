3db7a7b1544ae15e56ac2ced46818949079ef5be
0xnewton <0xnewton@protonmail.com>
2022-02-18 00:22:10 -0500

    nit: remove comments

--- src/lib/components/TicketCard/RedeemButton.tsx
+++ src/lib/components/TicketCard/RedeemButton.tsx
@@ -19,63 +19,6 @@ const RedeemButton = (props: RedeemButtonProps) => {
   const stateID = props.ticketID && snap.lootboxAddress && generateStateID(snap.lootboxAddress, props.ticketID)
   const ticket = stateID && snap.tickets[stateID] ? snap.tickets[stateID] : undefined
 
-  //     if (!isWalletConnected) {
-  //     return <WalletButton></WalletButton>
-  //   } else if (isWalletConnected && (!snapCrowdSaleState.inputToken.data || !snapCrowdSaleState.outputToken.data)) {
-  //     return (
-  //       <$Button
-  //         screen={screen}
-  //         backgroundColor={`${COLORS.surpressedBackground}40`}
-  //         color={`${COLORS.surpressedFontColor}80`}
-  //         style={{ fontWeight: 'lighter', cursor: 'not-allowed', minHeight: '60px', height: '100px' }}
-  //       >
-  //         {validChain ? 'Select a Token' : 'Switch Network'}
-  //       </$Button>
-  //     )
-  //   } else if (isInsufficientFunds) {
-  //     return (
-  //       <$Button
-  //         screen={screen}
-  //         backgroundColor={`${COLORS.surpressedBackground}40`}
-  //         color={`${COLORS.surpressedFontColor}80`}
-  //         style={{ fontWeight: 'lighter', cursor: 'not-allowed', minHeight: '60px', height: '100px' }}
-  //       >
-  //         Insufficient Funds
-  //       </$Button>
-  //     )
-  //   } else if (isInputAmountValid && !isAllowanceCovered) {
-  //     return (
-  //       <$Button
-  //         screen={screen}
-  //         onClick={approveStableCoinToken}
-  //         backgroundColor={`${COLORS.warningBackground}`}
-  //         color={`${COLORS.warningFontColor}`}
-  //         style={{ minHeight: '60px', height: '100px' }}
-  //         disabled={snapCrowdSaleState.ui.isButtonLoading}
-  //       >
-  //         <LoadingText
-  //           loading={snapCrowdSaleState.ui.isButtonLoading}
-  //           text="Approve Transaction"
-  //           color={COLORS.warningFontColor}
-  //         />
-  //       </$Button>
-  //     )
-  //   } else if (isInputAmountValid) {
-  //     return (
-  //       <$Button
-  //         screen={screen}
-  //         onClick={purchaseGuildToken}
-  //         backgroundColor={`${COLORS.trustBackground}C0`}
-  //         backgroundColorHover={`${COLORS.trustBackground}`}
-  //         color={COLORS.trustFontColor}
-  //         style={{ minHeight: '60px', height: '100px' }}
-  //         disabled={snapCrowdSaleState.ui.isButtonLoading}
-  //       >
-  //         <LoadingText loading={snapCrowdSaleState.ui.isButtonLoading} text="PURCHASE" color={COLORS.trustFontColor} />
-  //       </$Button>
-  //     )
-  //   }
-
   const toggleRoute = () => {
     const stateID = snap.lootboxAddress && props.ticketID && generateStateID(snap.lootboxAddress, props.ticketID)
     if (stateID && snap.tickets[stateID]) {
