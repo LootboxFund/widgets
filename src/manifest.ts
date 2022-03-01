1a2a4933f7a6aa3e41f13e243af78e5cd7511d00
0xterran <0xterran@protonmail.com>
2022-03-01 00:46:18 -0500

    0.2.0-demo Release patch

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,4 +1,4 @@
-import { latest as Manifest } from '@lootboxfund/manifest'
+import { Manifest_v0_2_0_demo as Manifest } from '@lootboxfund/manifest'
 export const manifest = Manifest.default
 
 export const STORAGE_URL = 'https://firebasestorage.googleapis.com/v0/b/guildfx-exchange.appspot.com'
