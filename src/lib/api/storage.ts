1b7489eb70c48d12bd0cecc47e42217a25d99027
0xnewton <0xnewton@protonmail.com>
2022-03-31 18:08:28 -0400

    remove un-used code in stroage file

--- src/lib/api/storage.ts
+++ src/lib/api/storage.ts
@@ -1,37 +1,7 @@
 import { ChainIDHex, ITicketMetadata, TicketID, Address, ContractAddress } from '@wormgraph/helpers'
 import { SemanticVersion } from '@wormgraph/manifest'
-import {
-  DEFAULT_CHAIN_ID_HEX,
-  storageUrl,
-  DEFAULT_TICKET_IMAGE,
-  DEFAULT_TICKET_BACKGROUND,
-  DEFAULT_TICKET_BACKGROUND_COLOR,
-} from 'lib/hooks/constants'
-import { userState } from 'lib/state/userState'
 import { manifest } from '../../manifest'
 import { encodeURISafe } from './helpers'
-
-const lootboxUrl = (lootboxAddress: Address) =>
-  `${storageUrl(userState.network.currentNetworkIdHex || DEFAULT_CHAIN_ID_HEX)}/lootbox/${lootboxAddress}`
-
-const metadataStorageUrl = (lootboxAddress: Address, ticketID: TicketID) =>
-  `${lootboxUrl(lootboxAddress)}/lootbox/${lootboxAddress}/ticket/${ticketID}.json?alt=media`
-
-const defaultMetadataStorageUrl = (lootboxAddress: Address) =>
-  `${lootboxUrl(lootboxAddress)}/lootbox/${lootboxAddress}/ticket/default.json?alt=media`
-
-export const readJSON = async <T>(file: string): Promise<T> => {
-  // TODO: dynamically read json file in cloud storage
-  const result: any = {
-    address: '________',
-    name: 'Artemis Guild',
-    description: '',
-    image: DEFAULT_TICKET_IMAGE,
-    backgroundColor: DEFAULT_TICKET_BACKGROUND_COLOR,
-    backgroundImage: DEFAULT_TICKET_BACKGROUND,
-  }
-  return result as T
-}
 
 export const getLootboxURI = async ({
   semver,
@@ -44,10 +14,8 @@ export const getLootboxURI = async ({
   lootboxAddress: ContractAddress
   bucket: string
 }) => {
-  const filePath = `v/${chainIdHex}/lootbox-uri/${lootboxAddress}.json`
-  const downloadablePath = `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${encodeURISafe(
-    filePath
-  )}?alt=media`
+  const filePath = `${bucket}/${chainIdHex}/${lootboxAddress}.json`
+  const downloadablePath = `${manifest.storage.downloadUrl}/${encodeURISafe(filePath)}?alt=media`
   const lootboxURI = await (await fetch(downloadablePath)).json()
   return lootboxURI
 }
@@ -56,14 +24,11 @@ export const readTicketMetadata = async (
   lootboxAddress: ContractAddress,
   ticketID: TicketID
 ): Promise<ITicketMetadata> => {
-  // const defaultFilepath = defaultMetadataStorageUrl(lootboxAddress)
-  // const filepath = metadataStorageUrl(lootboxAddress, ticketID)
-  // const { address, name, description, image, backgroundColor, backgroundImage } = await readJSON(filepath)
   const { address, name, description, image, backgroundColor, backgroundImage } = await getLootboxURI({
     lootboxAddress,
     semver: manifest.googleCloud.semver,
     chainIdHex: manifest.chain.chainIDHex,
-    bucket: manifest.googleCloud.bucket.id,
+    bucket: manifest.storage.buckets.lootboxUri.id,
   })
 
   return { address, name, description, image, backgroundColor, backgroundImage }
