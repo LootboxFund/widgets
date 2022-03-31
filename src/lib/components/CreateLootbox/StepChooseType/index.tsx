66783b092a3a20be322cb6c8b985a484f3cc5637
0xterran <0xterran@protonmail.com>
2022-03-30 17:55:40 -0700

    Lootbox type selection ready

---
+++ src/lib/components/CreateLootbox/StepChooseType/index.tsx
@@ -0,0 +1,234 @@
+import react, { forwardRef, useEffect, useState } from 'react'
+import styled from 'styled-components'
+import StepCard, { $StepHeading, $StepSubheading, StepStage } from 'lib/components/CreateLootbox/StepCard'
+import { $Horizontal, $Vertical } from 'lib/components/Generics'
+import { $Button } from 'lib/components/Generics/Button'
+import { COLORS, TYPOGRAPHY } from 'lib/theme'
+import { NetworkOption } from '../state'
+import WalletStatus from 'lib/components/WalletStatus'
+import HelpIcon from 'lib/theme/icons/Help.icon'
+import ReactTooltip from 'react-tooltip'
+import useWindowSize, { ScreenSize } from 'lib/hooks/useScreenSize'
+import { useSnapshot } from 'valtio'
+import { getUserBalanceOfNativeToken } from 'lib/hooks/useContract'
+import { userState } from 'lib/state/userState'
+import { Address } from '@wormgraph/helpers'
+
+export interface StepChooseTypeProps {
+  stage: StepStage
+  selectedNetwork?: NetworkOption
+  onSelectType: (type: LootboxType) => void
+  selectedType: LootboxType
+  onNext: () => void
+  setValidity: (bool: boolean) => void
+}
+
+export type LootboxType = 'escrow' | 'instant' | 'tournament'
+export const LOOTBOX_TYPE_OPTIONS = [
+  {
+    key: 'escrow' as LootboxType,
+    emoji: '🎁',
+    title: 'Escrow',
+    bio: 'Use this if you have a target amount to fundraise. You can only access the money if you hit your target goal.',
+    idealFor: 'Most Gamers, asset purchases, Guilds.',
+    helpText: '',
+  },
+  {
+    key: 'instant' as LootboxType,
+    emoji: '🎉',
+    title: 'Instant',
+    bio: 'Use this if you want to instantly get the money and continue fundraising while you play.',
+    idealFor: 'Streamers, Dungeon Crawls, Limitless spending',
+    helpText: '',
+  },
+  {
+    key: 'tournament' as LootboxType,
+    emoji: '⚔️',
+    title: 'Tournament',
+    bio: 'Use this if you want to participate in an ESports Tournament with a set buy-in. Acts like Escrow.',
+    idealFor: 'Casual & Competitive ESports',
+    helpText: '',
+  },
+]
+
+const StepChooseType = forwardRef((props: StepChooseTypeProps, ref: React.RefObject<HTMLDivElement>) => {
+  const { screen } = useWindowSize()
+  const isMobile = screen === 'tablet' || screen === 'mobile'
+  const snapUserState = useSnapshot(userState)
+  const [errors, setErrors] = useState<string[] | undefined>(undefined)
+  const [hasNonZeroTokens, setHasNonZeroToken] = useState<boolean>(false)
+
+  const getLootboxType = (type: LootboxType) => {
+    return LOOTBOX_TYPE_OPTIONS.find((option) => option.key === type)
+  }
+
+  const renderTypeOptions = () => {
+    const selectType = (type: LootboxType) => {
+      props.onSelectType(type)
+      props.setValidity(true)
+    }
+    return (
+      <$Vertical spacing={2}>
+        {LOOTBOX_TYPE_OPTIONS.map((option) => (
+          <$NetworkOption
+            isSelected={props.selectedType === option.key}
+            themeColor={props.selectedNetwork?.themeColor}
+            onClick={() => selectType(option.key)}
+            key={option.key}
+            isAvailable={true}
+          >
+            <$TypeIcon>{option.emoji}</$TypeIcon>
+
+            <$Horizontal flex={1} justifyContent="space-between">
+              <$NetworkName isAvailable={true} isSelected={props.selectedType === option.key}>
+                {option.title}
+              </$NetworkName>
+            </$Horizontal>
+          </$NetworkOption>
+        ))}
+      </$Vertical>
+    )
+  }
+  return (
+    <$StepChooseType>
+      {ref && <div ref={ref}></div>}
+      <StepCard
+        themeColor={props.selectedNetwork?.themeColor}
+        stage={props.selectedNetwork ? props.stage : 'in_progress'}
+        onNext={props.onNext}
+        errors={errors}
+      >
+        <$Wrapper screen={screen}>
+          <$Vertical flex={1}>
+            <$StepHeading>
+              2. Choose Lootbox Type
+              <HelpIcon tipID="stepNetwork" />
+              <ReactTooltip id="stepNetwork" place="right" effect="solid">
+                The network you choose should be the same blockchain as the game you intend to play. You may bridge
+                money across chains after funding, if needed.
+              </ReactTooltip>
+            </$StepHeading>
+            <$StepSubheading>Use the default option “Escrow” if you don’t know which to pick.</$StepSubheading>
+            <br />
+            {renderTypeOptions()}
+          </$Vertical>
+          <$Vertical flex={1} style={{ justifyContent: 'flex-end' }}>
+            <div
+              style={
+                isMobile
+                  ? {
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'flex-start',
+                      alignItems: 'flex-start',
+                      paddingBottom: '20px',
+                      flex: 1,
+                      width: '100%',
+                    }
+                  : {
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'flex-start',
+                      alignItems: 'flex-start',
+                    }
+              }
+            >
+              <$BigTypeOption screen={screen}>{getLootboxType(props.selectedType)?.emoji || '💰'}</$BigTypeOption>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'flex-start',
+                  alignItems: 'flex-start',
+                  paddingLeft: '20px',
+                }}
+              >
+                <$Horizontal>
+                  <$TypeTitle>{getLootboxType(props.selectedType)?.title}</$TypeTitle>
+                  <HelpIcon tipID={`stepType${props.selectedType}`} />
+                  <ReactTooltip id={`stepType${props.selectedType}`} place="right" effect="solid">
+                    {getLootboxType(props.selectedType)?.helpText}
+                  </ReactTooltip>
+                </$Horizontal>
+                <$StepSubheading>{getLootboxType(props.selectedType)?.bio}</$StepSubheading>
+                <span style={{ display: 'inline', marginTop: '10px' }}>
+                  <b style={{ color: COLORS.surpressedFontColor }}>Ideal for:</b>{' '}
+                  <$StepSubheading>{getLootboxType(props.selectedType)?.idealFor}</$StepSubheading>
+                </span>
+              </div>
+            </div>
+          </$Vertical>
+        </$Wrapper>
+      </StepCard>
+    </$StepChooseType>
+  )
+})
+
+const $StepChooseType = styled.section<{}>`
+  font-family: sans-serif;
+  width: 100%;
+  color: ${COLORS.black};
+`
+
+export const $TypeIcon = styled.span<{ size?: 'small' | 'medium' | 'large' }>`
+  font-size: ${(props) => (props.size === 'large' ? '2.2rem' : props.size === 'medium' ? '1.7rem' : '1.5rem')};
+`
+
+export const $BigTypeOption = styled.span<{ screen?: 'mobile' | 'tablet' | 'desktop' }>`
+  font-size: ${(props) => (props.screen === 'desktop' ? '4rem' : props.screen === 'tablet' ? '3rem' : '3rem')};
+  ${(props) => props.screen !== 'desktop' && `margin-left: 20px; margin-top: 30px; margin-bottom: 10px;`}
+`
+
+export const $Wrapper = styled.div<{ screen: ScreenSize }>`
+  flex: 1;
+  display: flex;
+  flex-direction: ${(props) => (props.screen === 'mobile' || props.screen === 'tablet' ? 'column' : 'row')};
+  justify-content: space-between;
+`
+
+const $TypeTitle = styled.span`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: ${COLORS.surpressedFontColor};
+  line-height: 2rem;
+`
+
+const $NetworkOption = styled.button<{ isAvailable?: boolean; themeColor?: string; isSelected?: boolean }>`
+  width: 300px;
+  padding: 8px 10px;
+  flex: 1;
+  display: flex;
+  border-radius: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  border: 0.5px solid #cdcdcd;
+  ${(props) => props.isAvailable && 'cursor: pointer'};
+  ${(props) =>
+    props.themeColor && props.isSelected
+      ? `background-color: ${props.themeColor}`
+      : props.isAvailable && 'background-color: white'};
+  ${(props) => !props.isSelected && props.isAvailable && 'box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);'}
+`
+
+const $NetworkName = styled.span<{ isAvailable?: boolean; isSelected?: boolean }>`
+  font-size: ${TYPOGRAPHY.fontSize.large};
+  ${(props) => props.isAvailable && 'text-transform: uppercase'};
+  ${(props) =>
+    props.isAvailable ? `font-weight: ${TYPOGRAPHY.fontWeight.bold}` : `font-weight: ${TYPOGRAPHY.fontWeight.light}`};
+  margin-left: 20px;
+  ${(props) => props.isSelected && 'color: white'};
+`
+
+const $ComingSoon = styled.span<{ isSelected?: boolean }>`
+  font-size: ${TYPOGRAPHY.fontSize.xsmall};
+  color: ${(props) => (props.isSelected ? COLORS.white : COLORS.surpressedFontColor)};
+  text-transform: uppercase;
+  font-weight: ${TYPOGRAPHY.fontWeight.bold};
+  line-height: 0.6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`
+
+export default StepChooseType
