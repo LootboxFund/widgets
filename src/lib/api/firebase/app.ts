5a4aaae2ad3b5610c6db2196be57ea35f0beeff4
0xterran <0xterran@protonmail.com>
2022-04-08 04:54:25 -0700

    0.2.8-firebase

--- src/lib/api/firebase/app.ts
+++ src/lib/api/firebase/app.ts
@@ -1,16 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
+import { manifest } from '../../../manifest'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCE5gn-Tm4fwzOHH-wpx68SxUk_-BuH5BU',
-  authDomain: 'guildfx-exchange.firebaseapp.com',
-  databaseURL: 'https://guildfx-exchange-default-rtdb.firebaseio.com',
-  projectId: 'guildfx-exchange',
-  storageBucket: 'guildfx-exchange.appspot.com',
-  messagingSenderId: '334494020080',
-  appId: '1:334494020080:web:4baa825fb0923fbab0ad35',
-  measurementId: 'G-2GNQPQ3LRX',
-}
+const firebaseConfig = manifest.firebase
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
