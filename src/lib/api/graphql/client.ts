920ace2f43e5618f2a8c7b13ee2dea0815031edf
0xnewton <0xnewton@protonmail.com>
2022-06-01 19:19:43 -0400

    pass firebase bearer into client

--- src/lib/api/graphql/client.ts
+++ src/lib/api/graphql/client.ts
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { manifest } from 'manifest'
+import { auth } from '../firebase/app'
+
+const httpLink = createHttpLink({
+  uri: manifest.cloudRun.containers.lootboxServer.fullRoute || 'http://localhost:8080/graphql',
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await auth.currentUser?.getIdToken(/* forceRefresh */ true)
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 
 const client = new ApolloClient({
-  uri: manifest.cloudRun.containers.lootboxServer.fullRoute || 'http://localhost:8080/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   name: 'LootboxWidgets',
   version: '1.0',
