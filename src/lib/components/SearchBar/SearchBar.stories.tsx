7cce583d7e272a84e601d921240d9018ab9f87f3
0xnewton <0xnewton@protonmail.com>
2022-07-15 02:50:51 -0400

    lootbox feed + searh bar intl

--- src/lib/components/SearchBar/SearchBar.stories.tsx
+++ src/lib/components/SearchBar/SearchBar.stories.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import SearchBar, { TemplateProps } from 'lib/components/SearchBar'
+import LocalizationWrapper from '../Internationalization/LocalizationWrapper'
 
 export default {
   title: 'SearchBar',
   component: SearchBar,
 }
 
-const Demo = (args: TemplateProps) => <SearchBar {...args} />
+const Demo = (args: TemplateProps) => (
+  <LocalizationWrapper>
+    <SearchBar {...args} />
+  </LocalizationWrapper>
+)
 
 export const Basic = Demo.bind({})
 Basic.args = {}
--- src/lib/components/SearchBar/index.tsx
+++ src/lib/components/SearchBar/index.tsx
@@ -1,31 +1,39 @@
-import { COLORS, ContractAddress } from '@wormgraph/helpers';
+import { COLORS, ContractAddress } from '@wormgraph/helpers'
 import useWindowSize from 'lib/hooks/useScreenSize'
 import react, { useState } from 'react'
 import styled from 'styled-components'
 import $Button from '../Generics/Button'
 import $Input from '../Generics/Input'
 import { validateErc20 } from '../RewardSponsors'
-import { manifest } from 'manifest';
+import { manifest } from 'manifest'
+import useWords from 'lib/hooks/useWords'
 
 export interface TemplateProps {}
 const SearchBar = (props: TemplateProps) => {
+  const words = useWords()
   const { screen } = useWindowSize()
   const [searchString, setSearchString] = useState('')
   const validAddress = validateErc20(searchString as ContractAddress)
-	return (
+  return (
     <$SearchBar>
       <$Input
         value={searchString}
         onChange={(e) => setSearchString(e.target.value)}
         screen={screen}
-        style={{ backgroundColor: `${COLORS.surpressedBackground}1A`, color: COLORS.surpressedFontColor, fontWeight: 'lighter', height: '100%', flex: 5 }}
+        style={{
+          backgroundColor: `${COLORS.surpressedBackground}1A`,
+          color: COLORS.surpressedFontColor,
+          fontWeight: 'lighter',
+          height: '100%',
+          flex: 5,
+        }}
         placeholder={'Enter Lootbox Address'}
       ></$Input>
       <$Button
         screen={screen}
         disabled={!validAddress}
         color={`${COLORS.white}`}
-        onClick={() => window.open(`${manifest.microfrontends.webflow.lootboxUrl}?lootbox=${searchString}`, "_self")}
+        onClick={() => window.open(`${manifest.microfrontends.webflow.lootboxUrl}?lootbox=${searchString}`, '_self')}
         backgroundColor={validAddress ? `${COLORS.trustBackground}` : `${COLORS.trustBackground}2A`}
         style={{
           height: '100%',
@@ -34,13 +42,13 @@ const SearchBar = (props: TemplateProps) => {
           fontSize: screen === 'desktop' ? '1.5rem' : '1rem',
           marginLeft: '5px',
           width: '100px',
-          padding: screen === 'desktop' || screen === 'tablet' ? '' : '0px 10px'
+          padding: screen === 'desktop' || screen === 'tablet' ? '' : '0px 10px',
         }}
       >
-        Search
+        {words.search}
       </$Button>
-		</$SearchBar>
-	)
+    </$SearchBar>
+  )
 }
 
 const $SearchBar = styled.div<{}>`
@@ -49,4 +57,4 @@ const $SearchBar = styled.div<{}>`
   width: 100%;
 `
 
-export default SearchBar;
+export default SearchBar
