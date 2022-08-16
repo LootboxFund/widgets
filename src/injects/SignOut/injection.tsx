edfdcb975b497ed23359ac80a7127d0842383412
0xnewton <0xnewton@protonmail.com>
2022-08-16 04:34:41 -0400

    fix bug

--- src/injects/SignOut/injection.tsx
+++ src/injects/SignOut/injection.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SignOut from 'lib/components/SignOut'
+import { ApolloProvider } from '@apollo/client'
+import client from 'lib/api/graphql/client'
+import LocalizationWrapper from 'lib/components/LocalizationWrapper'
 
 export const inject = () => {
   const targetInjectionPoint = document.getElementById('sign-out')
   ReactDOM.render(
     <React.StrictMode>
-      <SignOut />
+      <ApolloProvider client={client}>
+        <LocalizationWrapper>
+          <SignOut />
+        </LocalizationWrapper>
+      </ApolloProvider>
     </React.StrictMode>,
     targetInjectionPoint
   )
