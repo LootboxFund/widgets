644e2daf9c734af8bb1f1868f563d38d84aaad1d
0xnewton <0xnewton@protonmail.com>
2022-08-24 19:21:20 -0400

    add my tickets

---
+++ scripts/deploy-my-tickets.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = new express()
+const { latest: Manifest } = require('@wormgraph/manifest')
+const manifest = Manifest.default
+const { uploadFile } = require('./upload-file')
+
+/**
+ * CONSTANTS
+ *
+ */
+const bucketName = manifest.storage.buckets.widgets.id || 'guildfx-exchange.appspot.com'
+const semver = manifest.semver.id || '0.2.0-sandbox'
+const absPath = '/users/starship420/repo/lootbox/widgets/iife/'
+
+console.log(`
+
+  Uploading ${semver} to ${bucketName}
+
+  env: ${process.env.NODE_ENV}
+`)
+
+const MyTickets = process.env.NODE_ENV === 'production' ? 'MyTickets.production.js' : 'MyTickets.js'
+
+const fileNames = [MyTickets]
+
+fileNames.map((filename) => {
+  uploadFile({
+    filename,
+    semver,
+    absPath,
+    bucketName,
+  })
+})
+
+app.listen(process.env.PORT || 8088, () => {
+  console.log('node server running')
+})
