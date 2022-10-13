5cbbd1c35c4f228dec00995804be19f5d866de76
0xterran <0xterran@protonmail.com>
2022-10-13 07:05:33 +0700

    fixed the phone auth

--- src/lib/components/ViralOnboarding/index.tsx
+++ src/lib/components/ViralOnboarding/index.tsx
@@ -41,8 +41,7 @@ const ViralOnboarding = (props: ViralOnboardingProps) => {
         return (
           <AddEmail
             onNext={() => {
-              setRoute('success')
-              // setRoute('sign-up')
+              setRoute('sign-up')
             }}
             onBack={() => setRoute('browse-lottery')}
           />
