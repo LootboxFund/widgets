e4c2c9f23432d1e8b7d9fccd34ae0c7cf5a992ff
0xnewton <0xnewton@protonmail.com>
2022-11-13 10:10:43 -0500

    remove unneeded noob cup page

--- src/lib/components/ViralOnboarding/components/AdVideoBeta2.tsx
+++ src/lib/components/ViralOnboarding/components/AdVideoBeta2.tsx
@@ -1,23 +1,20 @@
 import { $span, $Vertical, $ViralOnboardingCard, $ViralOnboardingSafeArea } from 'lib/components/Generics'
-import { TEMPLATE_LOOTBOX_STAMP, DEFAULT_NOOB_CUP_INFOGRAPHIC } from 'lib/hooks/constants'
+import { TEMPLATE_LOOTBOX_STAMP } from 'lib/hooks/constants'
 import { useViralOnboarding } from 'lib/hooks/useViralOnboarding'
 import useWords from 'lib/hooks/useWords'
 import styled from 'styled-components'
-import { $Heading, $NextButton, $SlideInFooter, $SubHeading } from '../contants'
-import { useEffect, useRef, useState } from 'react'
+import { $Heading, $NextButton, $SlideInFooter } from '../contants'
+import { useRef } from 'react'
 import { convertFilenameToThumbnail } from 'lib/utils/storage'
 import Video from './Video'
 import { VideoJsPlayer, VideoJsPlayerOptions } from 'video.js'
 import { LoadingText } from 'lib/components/Generics/Spinner'
-import { AdID, AdSetID, AffiliateID, ClaimID, COLORS, FlightID, OfferID, TYPOGRAPHY } from '@wormgraph/helpers'
+import { AdID, AdSetID, AffiliateID, COLORS, FlightID, OfferID, TYPOGRAPHY } from '@wormgraph/helpers'
 import { loadAdTrackingPixel } from 'lib/utils/pixel'
 import { AdEventAction } from 'lib/api/graphql/generated/types'
 import { useAuth } from 'lib/hooks/useAuth'
-import { manifest } from 'manifest'
-import { Affiliate } from '../../../api/graphql/generated/types'
 
 const DEFAULT_THEME_COLOR = COLORS.trustBackground
-const startingTime = 30 // seconds
 
 interface Props {
   nextUrl: string
@@ -29,32 +26,6 @@ const AdVideoBeta2 = (props: Props) => {
   const { referral, chosenPartyBasket, chosenLootbox, ad, sessionId, claim } = useViralOnboarding()
   const words = useWords()
   const { user } = useAuth()
-  const [timeLeft, setTimeLeft] = useState<number | null>(null)
-  const [isFinalScreen, setIsFinalScreen] = useState(false)
-
-  useEffect(() => {
-    if (isFinalScreen) {
-      setTimeLeft(startingTime)
-    }
-  }, [isFinalScreen])
-
-  useEffect(() => {
-    if (timeLeft === null) {
-      return
-    }
-    if (timeLeft === 0) {
-      // Write that the timer ran out in the database
-      // TODO
-      console.log('timer ran out')
-    }
-    if (timeLeft > 0) {
-      const interval = setInterval(() => {
-        setTimeLeft(timeLeft - 1)
-      }, 1000)
-      return () => clearInterval(interval)
-    }
-    return
-  }, [timeLeft])
 
   const themeColor = ad?.creative?.themeColor || DEFAULT_THEME_COLOR
 
@@ -92,10 +63,6 @@ const AdVideoBeta2 = (props: Props) => {
       : [],
   }
 
-  const toggleFinalScreen = () => {
-    setIsFinalScreen(!isFinalScreen)
-  }
-
   const handlePlayerReady = (player: VideoJsPlayer) => {
     playerRef.current = player
 
@@ -125,7 +92,7 @@ const AdVideoBeta2 = (props: Props) => {
 
     player.on('ended', () => {
       console.log('DONE!')
-      setIsFinalScreen(true)
+      props.onNext()
     })
   }
 
@@ -169,69 +136,69 @@ const AdVideoBeta2 = (props: Props) => {
     }
   }
 
-  if (isFinalScreen) {
-    return (
-      <$ViralOnboardingCard style={{ position: 'relative' }} backgroundColor="#ffffff">
-        <$ViralOnboardingSafeArea style={{ paddingTop: '0rem', overflowY: 'hidden' }}>
-          <$Vertical>
-            <$Vertical>
-              <$Heading style={{ color: themeColor, marginBottom: '0px' }}>
-                {`0:${String(timeLeft).length > 1 ? String(timeLeft) : '0' + String(timeLeft)}`}
-              </$Heading>
-              <$SubHeading
-                style={{
-                  color: themeColor,
-                  textTransform: 'uppercase',
-                  fontWeight: TYPOGRAPHY.fontWeight.light,
-                  margin: '0.6rem auto',
-                }}
-              >
-                OFFER ENDS
-              </$SubHeading>
-            </$Vertical>
-            <$Infographic
-              src={ad?.creative?.infographicLink || DEFAULT_NOOB_CUP_INFOGRAPHIC}
-              alt="Earn REAL MONEY in the Beginner's Cup"
-            />
-          </$Vertical>
-        </$ViralOnboardingSafeArea>
-        <$SlideInFooter themeColor={themeColor} preventDefault delay="0s">
-          <$Vertical spacing={2}>
-            <$NextButton
-              onClick={onCallToActionClick}
-              color={ad?.creative.themeColor || COLORS.trustBackground}
-              backgroundColor={COLORS.white}
-              style={{ marginTop: '30px', marginLeft: '15px', marginRight: '15px' }}
-              disabled={false}
-            >
-              <LoadingText
-                loading={false}
-                color={ad?.creative.themeColor || COLORS.white}
-                text={ad?.creative.callToAction || 'Download Game'}
-              />
-            </$NextButton>
-            <a
-              href={props.nextUrl || `${manifest.microfrontends.webflow.publicProfile}?uid=${user?.id}`}
-              style={{ textAlign: 'center' }}
-            >
-              <$span
-                style={{
-                  color: COLORS.white,
-                  textDecoration: 'underline',
-                  fontStyle: 'italic',
-                  textAlign: 'center',
-                  fontWeight: TYPOGRAPHY.fontWeight.light,
-                  cursor: 'pointer',
-                }}
-              >
-                skip
-              </$span>
-            </a>
-          </$Vertical>
-        </$SlideInFooter>
-      </$ViralOnboardingCard>
-    )
-  }
+  // if (isFinalScreen) {
+  //   return (
+  //     <$ViralOnboardingCard style={{ position: 'relative' }} backgroundColor="#ffffff">
+  //       <$ViralOnboardingSafeArea style={{ paddingTop: '0rem', overflowY: 'hidden' }}>
+  //         <$Vertical>
+  //           <$Vertical>
+  //             <$Heading style={{ color: themeColor, marginBottom: '0px' }}>
+  //               {`0:${String(timeLeft).length > 1 ? String(timeLeft) : '0' + String(timeLeft)}`}
+  //             </$Heading>
+  //             <$SubHeading
+  //               style={{
+  //                 color: themeColor,
+  //                 textTransform: 'uppercase',
+  //                 fontWeight: TYPOGRAPHY.fontWeight.light,
+  //                 margin: '0.6rem auto',
+  //               }}
+  //             >
+  //               OFFER ENDS
+  //             </$SubHeading>
+  //           </$Vertical>
+  //           <$Infographic
+  //             src={ad?.creative?.infographicLink || DEFAULT_NOOB_CUP_INFOGRAPHIC}
+  //             alt="Earn REAL MONEY in the Beginner's Cup"
+  //           />
+  //         </$Vertical>
+  //       </$ViralOnboardingSafeArea>
+  //       <$SlideInFooter themeColor={themeColor} preventDefault delay="0s">
+  //         <$Vertical spacing={2}>
+  //           <$NextButton
+  //             onClick={onCallToActionClick}
+  //             color={ad?.creative.themeColor || COLORS.trustBackground}
+  //             backgroundColor={COLORS.white}
+  //             style={{ marginTop: '30px', marginLeft: '15px', marginRight: '15px' }}
+  //             disabled={false}
+  //           >
+  //             <LoadingText
+  //               loading={false}
+  //               color={ad?.creative.themeColor || COLORS.white}
+  //               text={ad?.creative.callToAction || 'Download Game'}
+  //             />
+  //           </$NextButton>
+  //           <a
+  //             href={props.nextUrl || `${manifest.microfrontends.webflow.publicProfile}?uid=${user?.id}`}
+  //             style={{ textAlign: 'center' }}
+  //           >
+  //             <$span
+  //               style={{
+  //                 color: COLORS.white,
+  //                 textDecoration: 'underline',
+  //                 fontStyle: 'italic',
+  //                 textAlign: 'center',
+  //                 fontWeight: TYPOGRAPHY.fontWeight.light,
+  //                 cursor: 'pointer',
+  //               }}
+  //             >
+  //               skip
+  //             </$span>
+  //           </a>
+  //         </$Vertical>
+  //       </$SlideInFooter>
+  //     </$ViralOnboardingCard>
+  //   )
+  // }
 
   return (
     <$ViralOnboardingCard style={{ position: 'relative' }}>
@@ -291,7 +258,7 @@ const AdVideoBeta2 = (props: Props) => {
               fontWeight: TYPOGRAPHY.fontWeight.light,
               cursor: 'pointer',
             }}
-            onClick={toggleFinalScreen}
+            onClick={props.onNext}
           >
             skip
           </$span>
@@ -300,10 +267,6 @@ const AdVideoBeta2 = (props: Props) => {
     </$ViralOnboardingCard>
   )
 }
-
-const $FloatingContainer = styled.div`
-  position: relative;
-`
 
 const $PaddingWrapper = styled.div`
   padding: 0px 3rem;
@@ -315,22 +278,6 @@ const $PartyBasketImage = styled.img`
   object-fit: contain;
   margin: 20px auto;
   filter: drop-shadow(0px 0px 25px #ffffff);
-`
-
-const $OpacityGradient = styled.div`
-  height: 50px;
-  top: -50px;
-  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.65) 80%);
-  position: absolute;
-  width: 100%;
-`
-
-const $Video = styled.video`
-  width: 100%;
-  height: 100%;
-  max-width: 600px;
-  position: absolute;
-  object-fit: cover;
 `
 
 const $FloatingCover = styled.div`
@@ -366,10 +313,6 @@ const $FloatingCover = styled.div`
 const $CenteredContent = styled.div`
   position: relative;
   left: -50%;
-`
-
-const $Emblem = styled.img`
-  max-width: 240px;
 `
 
 const $ContainerSlide = styled.div<{ slideOn?: boolean; slideOff?: boolean; delay?: string[] }>`
@@ -418,11 +361,6 @@ const $ContainerSlide = styled.div<{ slideOn?: boolean; slideOff?: boolean; dela
       left: 50%;
     }
   } ;
-`
-
-const $Infographic = styled.img`
-  width: 100%;
-  margin-bottom: 150px;
 `
 
 export default AdVideoBeta2
