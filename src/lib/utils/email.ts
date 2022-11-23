086448a8daf30dcec107723716a1c8d9fa5cf217
0xnewton <0xnewton@protonmail.com>
2022-11-22 21:06:50 -0500

    better email truncation

--- src/lib/utils/email.ts
+++ src/lib/utils/email.ts
@@ -1,3 +1,4 @@
 export const truncateEmail = (email: string) => {
-  return email.replace(/^(.)(.*)(.@.*)$/, (_, a, b, c) => a + b.replace(/./g, '*') + c)
+  //   return email.replace(/^(.)(.*)(.@.*)$/, (_, a, b, c) => a + b.replace(/./g, '*') + c)
+  return email.replace(/^(.)(.*)(.@.*)$/, (_, a, _b, c) => a + '*****' + c)
 }
